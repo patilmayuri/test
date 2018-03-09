931834ab463588e9fe7bd2b3ed91bb4b1a0f50d4
patilmayuri <mayuri.patil@reancloud.com>
2018-03-09 14:49:51 +0530

    testing

--- service.py
+++ service.py
@@ -5,12 +5,6 @@ import logging
 def handler(data, context):
     """Handle an incoming HTTP request, it could be from reanbot-router or from any bot. """
     # readEvent
-    logger.info("incoming data : " + str(data))
+    print "hello......"
     # print("incoming data : " + str(data))
 
-    if "challenge" in data:
-        return data["challenge"]
-
-
-
-    return "500 OK"
